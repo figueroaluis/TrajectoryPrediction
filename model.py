80a5230c3acdcf1a4d6cb9f86ce52469190721da
Dhruv Vashisht <dvashish@andrew.cmu.edu>
2021-04-15 20:12:36 -0400

    Baseline model - complete, not run

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 class CNN(nn.Module):
     def __init__(self, input_width = 720, input_height = 576):
+        super(CNN, self).__init__()
         self.cnn = nn.Sequential(
             nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=3),
             nn.MaxPool2D(kernel_size = 3, stride = 2),
@@ -27,17 +28,21 @@ class CNN(nn.Module):
         feats = torch.flatten(cnn_feats)
         return self.dense(feats)
 
-class SceneGRU(nn.Module):
+class SceneGRU(nn.Module):    
     def __init__(self, hidden_dim = 128):
-        self.sceneGRU = nn.GRU(input_size = 512, hidden_size = hidden_dim, batch_first = True, dropout = 0.2) #why input = 512, batch_first?
+        super(SceneGRU, self).__init__()
+        self.cnn = CNN()
+        self.sceneGRU = nn.GRU(input_size = 256, hidden_size = hidden_dim, batch_first = True, dropout = 0.2) #why input = 512, batch_first?
     
     def forward(self, input):
-        return self.sceneGRU(input)
+        cnn_features = self.cnn(input)
+        return self.sceneGRU(cnn_features)[1]
 
-class GroupGRU(nn.Module):
+class GroupGRU(nn.Module):    
     def __init__(self, hidden_dim = 128, neighborhood_radius = 32, grid_radius = 4, grid_angle = 45):
+        super(GroupGRU, self).__init__()
         self.dense = nn.Sequential(
-            nn.Linear(64, hidden_dim),
+            nn.Linear(64, 64),
             nn.ReLU(inplace = True)
         )
 
@@ -47,20 +52,44 @@ class GroupGRU(nn.Module):
 
     def forward(self, input):
         features = self.dense(input)
-        return self.groupGRU(features)
+        return self.groupGRU(features)[1]
 
 
 class PersonGRU(nn.Module):
-    def __init__(self, hidden_dim, neighborhood_radius, grid_radius, grid_angle):
+    def __init__(self, hidden_dim):
+        super(PersonGRU, self).__init__()
         self.dense = nn.Sequential(
-            nn.Linear(64, hidden_dim),
+            nn.Linear(2, 64),
             nn.ReLU(inplace = True)
         )
 
-        self.groupGRU = nn.GRU(input_size = int(neighborhood_radius / grid_radius) * int(360 / grid_angle), 
-                                hidden_size = hidden_dim, batch_first = True, dropout = 0.2) # how will this interact with previous layer, batch_first?
+        self.personGRU = nn.GRU(input_size = 64, hidden_size = hidden_dim, batch_first = True, dropout = 0.2) # how will this interact with previous layer, batch_first?
 
 
     def forward(self, input):
         features = self.dense(input)
-        return self.groupGRU(features)
+        return self.personGRU(features)[1]
+
+class Encoder(nn.Module):
+    def __init__(self, hidden_dim = 128, neighborhood_radius = 32, grid_radius = 4, grid_angle = 45):
+        super(Encoder, self).__init__()
+        self.personModel = PersonGRU(hidden_dim)
+        self.groupModel = GroupGRU(hidden_dim, neighborhood_radius, grid_radius, grid_angle)
+        self.sceneModel = SceneGRU(hidden_dim)
+
+    def forward(self, images, group_features, person_features):
+        output = self.personModel(person_features)
+        output += self.groupModel(group_features)
+        output += self.sceneModel(images)
+        output = output.permute(1,0,2)
+        return output
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.decoderGRU = nn.GRU(input_size = 128, hidden_size = 128, batch_first = True, dropout = 0.2)
+        self.dense = nn.Linear(128, 2)
+    
+    def forward(self, encoder_feats):
+        decoder_out, _ = self.decoder(encoder_feats)
+        return self.dense(decoder_out)
