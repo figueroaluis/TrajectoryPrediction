1fa8c3022256e01fd502d4c5c4ec4ce784af2120
aplusc98 <asish1998@gmail.com>
2021-04-27 13:39:01 -0400

    main file and model push

--- main.py
+++ main.py
@@ -6,7 +6,40 @@ from data import BaseDataset
 from torch.utils.data import DataLoader
 import os
 from utils import DataProcesser
+from model import Model
 
+def validate(model, val_dataloader):
+    
+    for batch_id, data in enumerate(val_dataloader):
+
+        data['person_data'] = data['person_data'].cuda()
+        data['group_data'] = data['group_data'].cuda()
+        data['scene_data'] = data['scene_data'].cuda()
+        data['gt'] = data['gt'.cuda()]
+
+        op = model(data['scene_data'], data['group_data'], data['person_data'])
+    
+
+def train(model, criterion, optimizer, train_dataloader, val_dataloader, num_epochs):
+    model.train()
+    for epoch in num_epochs:
+        
+        for batch_id, data in enumerate(train_dataloader):
+
+            data['person_data'] = data['person_data'].cuda()
+            data['group_data'] = data['group_data'].cuda()
+            data['scene_data'] = data['scene_data'].cuda()
+            data['gt'] = data['gt'.cuda()]
+
+            optimizer.zero_grad()
+
+            op = model(data['scene_data'], data['group_data'], data['person_data'])
+            loss = criterion(op,data[])
+            loss.backward()
+            optimizer.step()
+        # model.eval()
+        # validate(model , val_dataloader)
+        print("Epoch: " + str(epoch), " Batch: " + str(batch_id) + " Loss: " + str(loss.item()))
 
 if __name__ == "__main__":
     # Feel free to add more args, or change/remove these.
@@ -29,5 +62,8 @@ if __name__ == "__main__":
 
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(weight_decay = 1e-5)
+
+    model = Model()
+    train(model,criterion, optimizer, trainloader, None, 10)
 
     
