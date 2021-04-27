3a985efde6efbd94d18f207b8ba71520e479b368
Dhruv Vashisht <dhruvv301292@gmail.com>
2021-04-27 10:59:16 -0400

    added util funciton calls

--- data.py
+++ data.py
@@ -2,21 +2,30 @@ import torch
 import torch.nn
 from torch.utils.data import Dataset
 import numpy as np
-from utils import *
+from utils import person_model_input, log_group_model_input, model_expected_ouput, preprocess
 
 class BaseDataset(Dataset):
-    def __init__(self, raw_obs_path, image_dir):
+    def __init__(self, obs_path, pred_path, raw_path, image_dir, image_size_dims = [720, 576],  neighborhood_size = 32, neighborhood_radius = 32, grid_radius = 4, grid_angle = 45):
         super().__init__()
-        self.raw_obs = np.load(raw_obs_path)
+        self.obs = np.load(obs_path) # obs_path should combine files from all sources or we should provide separate paths for separate datasets
+        self.pred = np.load(pred_path)
+
+        self.raw_data = preprocess(raw_path)
+
         self.image_dir = image_dir
         self.train_steps = 8
         self.pred_steps = 12
-        #TODO: call utils api to split data into person, group, scene data
-        self.person_data = 
-        self.group_data = None
+        #TODO: call utils api to split data into person, group, scene data        
+
+        self.person_data = person_model_input(self.obs, self.train_steps)        
+        self.group_data = log_group_model_input(self.obs, self.train_steps, neighborhood_size, image_size_dims, neighborhood_radius, grid_radius, grid_angle, [1, 1, 1, 1, 1, 1, 1, 1], self.raw_data)
         self.scene_data = None
+
+        self.ground_truth = model_expected_ouput(self.pred, self.pred_steps)
     
     def __len__(self):
         return len(self.raw_obs)
     
     def __getitem__(self, index):
+        
+
